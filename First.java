7ae01d1e41ef4999e6cd9ee0e0c9b48b752a35ac
eunseokchoi <cesboy@naver.com>
2016-07-29 10:15:27 +0900

    Add files via upload

---
+++ First.java
@@ -0,0 +1,15 @@
+package net.hb.day11;
+
+public class First {
+
+	public static void main(String[] args) {
+		
+		String data="1200";
+		int a=Integer.parseInt(data);
+		System.out.println(a);
+		String b="abcde";
+		data.charAt(3);
+		
+	}//main end
+
+}//class end
